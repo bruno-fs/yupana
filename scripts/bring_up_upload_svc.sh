7fcd6a2f3d64facd5459a61c12def2bbbc352485
Bruno Ciconelle <bciconel@redhat.com>
2022-06-03 11:50:55 -0300

    Adapt bring up upload svc script
    Current codebase for insights-ingress-go don't have a docker-compose.yml
    on the project root.

--- scripts/bring_up_upload_svc.sh
+++ scripts/bring_up_upload_svc.sh
@@ -4,6 +4,7 @@ set -x
 upload_dir_path='../../insights-ingress-go'
 cp -f config/.upload_env $upload_dir_path/.env
 cd $upload_dir_path
+ln -sf development/local-dev-start.yml docker-compose.yml
 . .env
 pipenv install --dev
 pipenv run docker-compose up --build
